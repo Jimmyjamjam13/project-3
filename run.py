4476dd632255a67be6c548049daf08aa2ab59cae
Jimmyjamjam13 <james13ryan@gmail.com>
2021-11-17 01:04:29 +0000

    add make guess and player choices def

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ scores = {"computer": 0, "player": 0}
 
 
 class Board:
-    
+
     """Will create player and computer board based on user input."""
 
     def __init__(self, name, size, ship_nums, type):
@@ -54,10 +54,10 @@ def valid_coordinates(x, y, board):
 
 
 def populate_board(board):
-    
+
     """Populates game board for each player, one for the user
     and one for the computer."""
-    
+
     print(f"{board.name}'s board:\n")
 
     size = board.size
@@ -70,3 +70,50 @@ def populate_board(board):
     board.print_board()
     print(board.ships)
 
+
+def make_guess(board):
+    
+    """For computer, generates random x and y coordanites.
+    For player, row and col input is requested."""
+
+    size = board.size
+    while True:
+        if board.type == 'computer':
+            print("Computer's turn to guess")
+            row_guess = random_number(board.size)
+            col_guess = random_number(board.size)
+        else:
+            row_guess = input('Enter row num:\n')
+            col_guess = input('Enter column num:\n')
+            
+        row = player_choices(str(row_guess), 0, size)
+        col = player_choices(str(col_guess), 0, size)
+
+        if row and col:
+            break
+
+    return [int(row_guess), int(col_guess)]
+
+
+def player_choices(values, a, b):
+ 
+    """ checks users guesses"""
+
+    board_guesses = board.guesses
+    while True:
+        for board_guess in board_guesses:
+            if (x, y) == board_guess:
+                print(f'{board.name}, you already guessed {(x, y)}.')
+                print('Please try again.')
+                return False
+
+        break
+
+    if (x, y) in other_board.ships:
+        board.guesses.append((x, y))
+        other_board.board[x][y] = '| O'
+        print('A Battleship has been hit!')
+    else:
+        board.guesses.append((x, y))
+        other_board.board[x][y] = '| X'
+        print('Missile missed target...')
